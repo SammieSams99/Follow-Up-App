6061bc77f92ceefde9f973e61318c6d229101eaf
Camille Bailey <59386910+Camille-SE@users.noreply.github.com>
2020-08-28 15:26:01 -0400

    Revert "added new form and my apps to navigation"

--- follow-up/src/App.test.js
+++ follow-up/src/App.test.js
@@ -1,4 +1,3 @@
-// what does this file do?
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
--- follow-up/src/index.js
+++ follow-up/src/index.js
@@ -1,29 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-// import Form from './components/Form'
-import './App.css'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// const form = {
-//   companyName: "Sony",
-//   jobTitle: "Software Engineer",
-//   location: "New York",
-//   jobType: "Full Time",
-//   appliedThrough: "Indeed",
-//   remindDate:"September 10, 2020"
-// }
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-
-
-
-//   <Form 
-//     companyName= {form.companyName}
-//     jobTitle= {form.jobTitle}
-//     location= {form.location}
-//     jobType= {form.jobType}
-//     appliedThrough= {form.appliedThrough}
-//     remindDate= {form.remindDate}
-//   />,
-
-ReactDOM.render(<App />, document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
